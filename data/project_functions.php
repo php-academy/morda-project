771d39f4d978cf863348d2bc0803045181848340
Pavel <pavelshvecov87@gmail.com>
2015-10-02 16:02:48 +0600

    Merge branch 'nasedkin' of https://github.com/php-academy/morda-project into shvetsov3

--- data/project_functions.php
+++ data/project_functions.php
@@ -22,3 +22,31 @@ function get_curr_city() {
 function set_curr_city( $curr_city ) {
     setcookie('curr_city', $curr_city, time()+ 60*60*24*30, '/');
 }
+
+function get_city_data_by_code($dbCity, $cityCode) {
+    foreach( $dbCity as $cityData ) {
+        if( $cityData['code'] == $cityCode ) {
+            return $cityData;
+        }
+    }
+    return false;
+}
+
+function get_city_name_by_code($dbCity, $cityCode) {
+    $cityData = get_city_data_by_code($dbCity,$cityCode);
+    if( $cityData ) {
+        return $cityData['name'];
+    } else {
+        return false;
+    }
+}
+
+function filter($dbAuto, $currCity) {
+    $result = array();
+    foreach( $dbAuto as $autoData ) {
+        if( $autoData['cityCode'] == $currCity ) {
+            $result[] = $autoData;
+        }
+    }
+    return $result;
+}
